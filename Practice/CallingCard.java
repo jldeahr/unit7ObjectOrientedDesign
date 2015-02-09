a5ffab6bae9a03f9e1a0bae2e7d720f950589d61
jldeahr <jldeahr50@gmail.com>
2015-02-09 12:19:09 -0600

    asdfa

--- Practice/CallingCard.java
+++ Practice/CallingCard.java
@@ -1,4 +1,49 @@
+
+
+/**
+ * Subclass of Card that holds an a card number and a pin
+ * 
+ * @author Matt Wright w/ Joe Deahr
+ * @version January 28, 2015
+ */
+public class CallingCard extends Card
 {
+    /** Holds the ID of the card */
+    private String number, pin;
+
     
+    /**
+     * Default constructor for objects of class CallingCard, accepting a name, number, and pin
+     */
+    public CallingCard(String n, String num, String pin)
+    {
+        super(n);
+        number = num;
+        this.pin = pin;
+    }
+    
+    public boolean equals(Card other)
+    {
+        if(this.toString().equals(other.toString()))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    /**
+     * Returns the formatted version of this class's description
+     *
+     * @return    description of this card
+     */
+    public String format()
+    {
+        return super.format() + "\nCard Number: " + this.number + " \nPin: " + this.pin;
+    }
+    
+    public String toString(){
+        return "DriverLicense[Name:\""+getName()+"\"][Number:"+number+",Pin:"+pin;
+    }
+
 }
-//im broken
